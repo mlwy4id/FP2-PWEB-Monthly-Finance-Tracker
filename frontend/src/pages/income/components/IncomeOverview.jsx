3a2e9f1cf24f319cecd7a36be122cc884be6c274
mlwy4id <azmiimaulawiysaid67@gmail.com>
2025-11-30 11:07:34 +0700

    refactor income overview

--- frontend/src/pages/income/components/IncomeOverview.jsx
+++ frontend/src/pages/income/components/IncomeOverview.jsx
@@ -2,7 +2,8 @@ import OverviewCard from "@/components/OverviewCard";
 import useIncome from "@/store/useIncomeStore";
 import { dailyRecap } from "@/utils/dailyRecap";
 import { monthlyRecap } from "@/utils/monthlyRecap";
-import { FaShoppingBag, FaCalendar } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+import { SunIcon, TrophyIcon } from "lucide-react";
 import { FaArrowTrendUp } from "react-icons/fa6";
 
 const IncomeOverview = () => {
@@ -12,27 +13,27 @@ const IncomeOverview = () => {
     <div className="flex justify-between items-center flex-wrap">
       <OverviewCard
         title="Total Income"
-        logo={<FaCalendar className="text-rose-700" size={18} />}
+        logo={<FaDollarSign className="text-green-600" size={18} />}
       >
-        <p className="text-red-800 text-3xl font-semibold tracking-tight">
+        <p className="text-green-700 text-3xl font-semibold tracking-tight">
           +Rp{monthlyRecap(incomes)}
         </p>
       </OverviewCard>
 
       <OverviewCard
         title="Income Today"
-        logo={<FaShoppingBag className="text-orange-700" size={18} />}
+        logo={<SunIcon className="text-teal-600" size={18} />}
       >
-        <p className="text-3xl text-orange-800 font-semibold tracking-tight">
+        <p className="text-3xl text-teal-700 font-semibold tracking-tight">
           +Rp{dailyRecap(incomes)}
         </p>
       </OverviewCard>
 
       <OverviewCard
         title="Top Source"
-        logo={<FaArrowTrendUp className="text-purple-700" size={18} />}
+        logo={<TrophyIcon className="text-emerald-600" size={18} />}
       >
-        <p className="text-3xl font-semibold text-purple-800">Freelance</p>
+        <p className="text-3xl font-semibold text-emerald-700">Freelance</p>
       </OverviewCard>
     </div>
   );
