76be82eeeeb2ca5c9be003d15930e612e021e7cb
mlwy4id <azmiimaulawiysaid67@gmail.com>
2025-11-30 08:41:07 +0700

    refactor RecentExpenseCard into reusable RecentCard

---
+++ frontend/src/components/RecentCard.jsx
@@ -0,0 +1,31 @@
+import RecentExpensesTable from "@/pages/expense/components/RecentExpensesTable";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const RECENTCARD_CONTENT = {
+  expense: RecentExpensesTable,
+};
+
+const RecentCard = ({ type, title, button, children }) => {
+  const RecentCardContent = RECENTCARD_CONTENT[type];
+
+  return (
+    <Card className={`w-full`}>
+      <CardHeader className={`flex justify-between items-center`}>
+        <CardTitle className={`text-xl font-semibold`}>
+          Recent {title}
+        </CardTitle>
+        {button}
+      </CardHeader>
+      <CardContent>{RecentCardContent && <RecentCardContent />}</CardContent>
+      <CardFooter className={`flex justify-end`}>{children}</CardFooter>
+    </Card>
+  );
+};
+
+export default RecentCard;
--- frontend/src/pages/expense/Expense.jsx
+++ frontend/src/pages/expense/Expense.jsx
@@ -1,23 +1,41 @@
 import PageLayout from "@/components/layouts/PageLayout";
-import RecentExpensesCard from "./components/RecentExpensesCard";
+import RecentCard from "@/components/RecentCard";
 import ExpenseOverview from "./components/ExpenseOverview";
-
-/*
-  Layouting
-  
-  [ Expense Bulan Ini ] [ Expense Hari Ini ] [ Kategori Terbanyak ]
-  [        Recent Expense (List)         ]
-  [        Expense by Category (Chart)   ]
-*/
+import { Button } from "@/components/ui/button";
+import { FaPlus } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import useModal from "@/store/useModalStore";
 
 const Expense = () => {
+  const openModal = useModal((state) => state.openModal);
+
   return (
     <PageLayout
       title="Expenses"
       subtitle="Monitor and categorize your spending"
     >
-      <ExpenseOverview/>
-      <RecentExpensesCard/>
+      <ExpenseOverview />
+      <RecentCard
+        type="expense"
+        title="Expenses"
+        button={
+          <Button
+            className={`bg-blue-600 hover:bg-blue-700`}
+            size={`lg`}
+            onClick={() => openModal("expense", "add")}
+          >
+            <FaPlus />
+            Add Expense
+          </Button>
+        }
+      >
+        <NavLink
+          to="/expense/history"
+          className="flex items-center gap-2 text-blue-600 underline"
+        >
+          View All
+        </NavLink>
+      </RecentCard>
     </PageLayout>
   );
 };
